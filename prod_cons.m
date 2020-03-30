7ee810639dc7089863ae2df7936813b01a6f2a8e
Lambis <lambiselef@gmail.com>
2020-03-30 17:45:05 +0300

    Update prod_cons.m

--- prod_cons.m
+++ prod_cons.m
@@ -2,29 +2,46 @@ clear;
 clc;
 close all;
 
-data = importdata('dataP6K10-20.csv');
-prodNum = 6;
+data = importdata('dataP1K100-200.csv');
+prodNum = 1;
 consNum = [1 2 4 8 16 32 64 128];
 n = length(consNum);
 
-dataMean = zeros(n,1);
+stats = zeros(n,6);
+stats(:,1) = consNum';
+
 for i = 1:n
+    
     dataTemp = data(i,:);
-    dataTemp = dataTemp(11:end);
-    dataTemp = dataTemp(dataTemp<100);
+    dataTemp = dataTemp(4801:end-200);
+    %dataTemp = dataTemp(dataTemp<100);
+       
+    stats(i,2) = mean(dataTemp);
+    stats(i,3) = std(dataTemp);
+    stats(i,4) = median(dataTemp);
+    stats(i,5) = min(dataTemp);
+    stats(i,6) = max(dataTemp);
+    
     figure();
     hold on;
-    histogram(dataTemp,80);
+    histogram(dataTemp);
     title(['#Producers=' num2str(prodNum) ' - #Consumers=' num2str(consNum(i))]);
     xlabel('Time(us)');
     ylabel('#Jobs');
+    %saveas(gcf,sprintf('Cons%d.png', stats(i,1)));
+
+    figure();
+    hold on;
+    plot(dataTemp,'.');
+    title(['#Producers=' num2str(prodNum) ' - #Consumers=' num2str(consNum(i))]);
     
-    dataMean(i) = mean(dataTemp);
+
 end
 
 figure();
 hold on;
-plot(consNum, dataMean);
-scatter(consNum, dataMean);
+plot(consNum, stats(:,2));
+scatter(consNum, stats(:,2));
 xlabel('#Consumers');
 ylabel('Average Waiting Time (us)');
+%saveas(gcf,'Average Waiting Time.png');
